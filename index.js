ebe6a2658b6463b79e32751f79a69c5c7a159108
Joe P Shoulak <joe@squarecompass.io>
2022-10-21 11:28:27 -0500

    particles spawn without overlapping

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+import { randomPosition, distanceTo } from "./lib/helper.js";
+import Particle from "./lib/Particle.js";
+
+const COLORS = ["#3288F0", "#34FA99", "#B1E33B", "#FABF34", "#F04C22"];
+const particleCount = 100;
+const padding = 5;
+
 /** @type {HTMLCanvasElement} */
 const canvas = document.getElementById("my-canvas");
 const c = canvas.getContext("2d");
@@ -5,15 +12,55 @@ const c = canvas.getContext("2d");
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
 
-const setup = (event) => {};
+let particles;
+
+const setup = () => {
+  particles = [];
+
+  for (let i = 0; i < particleCount; i++) {
+    let x, y;
+
+    do {
+      [x, y] = randomPosition();
+    } while (
+      particles.some((particle) => {
+        return (
+          distanceTo(particle.x, particle.y, x, y) <
+          particle.radius * 2 + padding
+        );
+      })
+    );
+
+    const color = COLORS[Math.floor(Math.random() * COLORS.length)];
+
+    particles.push(new Particle(c, x, y, Particle.radius, color));
+  }
+};
 
 const animate = () => {
   requestAnimationFrame(animate);
 
   c.clearRect(0, 0, innerWidth, innerHeight);
+
+  particles.forEach((particle) => particle.update());
 };
 
-window.addEventListener("resize", () => {});
+window.addEventListener("contextmenu", (event) => {
+  console.log("Reset called.");
+
+  event.preventDefault();
+
+  setup();
+});
+
+window.addEventListener("resize", () => {
+  console.log("Resize called.");
+
+  canvas.height = window.innerHeight;
+  canvas.width = window.innerWidth;
+
+  setup();
+});
 
 setup();
 animate();
